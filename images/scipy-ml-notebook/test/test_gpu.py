760699301a5844425fee8e186ed46645159c166a
jdkindley <jdkindley@ucsd.edu>
2022-02-15 13:10:30 -0800

    update gpu-test to use "untested" tag

--- images/scipy-ml-notebook/test/test_gpu.py
+++ images/scipy-ml-notebook/test/test_gpu.py
@@ -1,3 +1,4 @@
+import pytest
 import os
 from grpc_files.gpu_tester.client import *
 
@@ -5,8 +6,18 @@ EXAMPLE_GOOD_OUT='Test response :\n{\n  "torch": true,\n  "tensorflow": true,\n 
 EXAMPLE_MISSING_PACKAGE = "Test response :\nImage missing a necessary package: No module named 'tensorflow'\n"
 EXAMPLE_TIME_OUT = 'Test response :\n{\n  "torch": false,\n  "tensorflow": false,\n  "msg": "Test Failed: Timeout reached."\n}'
 
-def test_gpu_valid():
-    assert run(test_image=os.environ['TEST_IMAGE'],cer_path=None) == EXAMPLE_GOOD_OUT
+
+@pytest.fixture(scope='function')
+def untested_image():
+    image = os.environ['TEST_IMAGE']
+    repo,tag = image.split(":")
+    tag = 'untested'
+    return f'{repo}:{tag}'
+
+
+def test_gpu_valid(untested_image):
+
+    assert run(test_image=untested_image,cer_path=None) == EXAMPLE_GOOD_OUT
     
 def test_gpu_nonexistent_image():
     assert run(test_image='invalid_image_for_test',cer_path=None) == EXAMPLE_TIME_OUT
