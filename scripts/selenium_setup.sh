4117231552df6079ad397c218f546648540cc912
jdkindley <jdkindley@ucsd.edu>
2022-04-29 10:18:25 -0700

    update selenium chromedriver

--- scripts/selenium_setup.sh
+++ scripts/selenium_setup.sh
@@ -6,7 +6,7 @@ sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-k
 sudo bash -c "echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' >> /etc/apt/sources.list.d/google-chrome.list" 
 sudo apt -y update 
 sudo apt -y install google-chrome-stable 
-wget https://chromedriver.storage.googleapis.com/99.0.4844.51/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/101.0.4951.41/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip 
 sudo mv chromedriver /usr/bin/chromedriver 
 sudo chown root:root /usr/bin/chromedriver 
