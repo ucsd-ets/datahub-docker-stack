6dd53c1147643ccb1373b8b8c2677ce0759050c9
jdkindley <jdkindley@ucsd.edu>
2022-04-29 10:45:10 -0700

    move tests, add test for server extensions

--- images/datahub-base-notebook/test/test_start_container.py
+++ images/datahub-base-notebook/test/test_start_container.py
@@ -52,3 +52,66 @@ def test_tini_entrypoint(container, pid=1, command="tini"):
     cmd = c.exec_run(f"ps -p {pid} -o comm=")
     output = cmd.output.decode("utf-8").strip("\n")
     assert output == command, f"{command} shall be launched as pid {pid}, got {output}"
+
+@pytest.mark.parametrize(
+    "expected_server",
+    [
+        ("notebook"),
+    ],
+)
+def test_jupyter_lab_exists(container, http_client, expected_server):
+    """Check that the jupyter lab endpoint exists"""
+    LOGGER.info(f"Checking that jupyter lab endpoint exists when using jupyter {expected_server}")
+    c = container.run(
+        tty=True,
+        command=["start-notebook.sh"],
+    )
+    resp = http_client.get("http://localhost:8888/lab")
+    logs = c.logs(stdout=True).decode("utf-8")
+    LOGGER.debug(logs)
+    assert resp.status_code == 200, "Jupyter lab is not running"
+    assert (
+        f"Executing the command: jupyter {expected_server}" in logs
+    ), f"Not the expected command (jupyter {expected_server}) was launched"
+
+@pytest.mark.parametrize(
+    "expected_server",
+    [
+        ("notebook"),
+    ],
+)
+def test_jupyter_notebook_exists(container, http_client, expected_server):
+    """Check that the jupyter notebook endpoint exists"""
+    LOGGER.info(f"Checking that jupyter notebook endpoint exists when using jupyter {expected_server}")
+    c = container.run(
+        tty=True,
+        command=["start-notebook.sh"],
+    )
+    resp = http_client.get("http://localhost:8888/tree")
+    logs = c.logs(stdout=True).decode("utf-8")
+    LOGGER.debug(logs)
+    assert resp.status_code == 200, "Jupyter notebook(/tree) is not running "
+    assert (
+        f"Executing the command: jupyter {expected_server}" in logs
+    ), f"Not the expected command (jupyter {expected_server}) was launched"
+
+@pytest.mark.parametrize(
+    "expected_server",
+    [
+        ("notebook"),
+    ],
+)
+def test_server_extensions_start(container, http_client, expected_server):
+    """Check that the server extensions start"""
+    LOGGER.info(f"Checking that server extensions start when using jupyter {expected_server}")
+    c = container.run(
+        tty=True,
+        command=["start-notebook.sh"],
+    )
+    resp = http_client.get("http://localhost:8888")
+    logs = c.logs(stdout=True).decode("utf-8")
+    LOGGER.debug(logs)
+    assert resp.status_code == 200, "Server is not listening"
+    assert (
+        f"Error loading server extension" not in logs
+    ), f"Server Extension(s) failed to start:\n{logs}"
