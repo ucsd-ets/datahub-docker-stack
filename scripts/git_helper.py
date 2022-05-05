b089d85c40fe8cacba1ce27e5a7b5c8c243027e3
jdkindley <jdkindley@ucsd.edu>
2022-05-05 11:02:09 -0700

    dummy edits

--- scripts/git_helper.py
+++ scripts/git_helper.py
@@ -28,7 +28,7 @@ class GitHelper:
     @staticmethod
     def commit_changed_files() -> list:
         print(environ['GITHUB_REF_NAME'])
-        if (environ['GITHUB_REF_NAME'] == "origin/main"):
+        if (environ['GITHUB_REF_NAME'] == "main"):
             return git['log', '-m', '-1', '--name-only', '--pretty="format:"']().split()
         return git['diff', 'origin/main', '--name-only']().split()
 
