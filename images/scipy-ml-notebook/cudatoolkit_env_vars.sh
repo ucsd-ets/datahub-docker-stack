0c6bafca837dbd6cbf2c77e1e9ce7796cd484855
wesuuu <wuykimpang@ucsd.edu>
2022-12-06 16:00:59 -0800

    Updated ld scripts

--- images/scipy-ml-notebook/cudatoolkit_env_vars.sh
+++ images/scipy-ml-notebook/cudatoolkit_env_vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # set systemwide env variables for Conda package: cudnn
-cudnn_version=$(conda list 2> /dev/null | grep cudnn | awk -F' ' '{ print $2 "-" $3 }')
+cudatoolkit=$(conda list 2> /dev/null | grep cudatoolkit | awk -F' ' '{ print $2 "-" $3 }')
 
-export LD_LIBRARY_PATH=/opt/conda/pkgs/cudnn-$cudnn_version/lib/:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=/opt/conda/pkgs/cudatoolkit-$cudatoolkit/lib:${LD_LIBRARY_PATH}
