8c3d264b164e237e133f48f64737f4702f22ab9c
wesuuu <wuykimpang@ucsd.edu>
2022-12-12 14:29:16 -0800

    Added more nbgrader install

--- images/datahub-base-notebook/scripts/install-python/install-nbgrader.sh
+++ images/datahub-base-notebook/scripts/install-python/install-nbgrader.sh
@@ -2,5 +2,9 @@
 
 pip install nbgrader==0.8.1
 
+jupyter nbextension install --symlink --sys-prefix --py nbgrader
+jupyter nbextension enable --sys-prefix --py nbgrader
+jupyter serverextension enable --sys-prefix --py nbgrader
+
 jupyter labextension enable --level=system nbgrader
 jupyter server extension enable --system --py nbgrader
