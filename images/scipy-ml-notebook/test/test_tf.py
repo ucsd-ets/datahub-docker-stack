a428cdc935c4f2e614ecd705cad71910a8dc92fc
Thomaswang0822 <thomaswang0822@outlook.com>
2022-12-14 14:52:16 -0800

    Add logs; change waiting time

--- images/scipy-ml-notebook/test/test_tf.py
+++ images/scipy-ml-notebook/test/test_tf.py
@@ -12,8 +12,6 @@ THIS_DIR = os.path.dirname(os.path.realpath(__file__))
     "test_file",
     ["test_tf"],
 )
-
-
 def test_tf(container, test_file):
     """ test_tf.ipynb checks
     1. tensorboard extention can be loaded.
@@ -22,7 +20,7 @@ def test_tf(container, test_file):
     host_data_dir = os.path.join(THIS_DIR, "data")
     cont_data_dir = "/home/jovyan/data"
     output_dir = "/tmp"
-    timeout_sec = 60
+    timeout_sec = 600
     LOGGER.info(f"Test that {test_file} notebook can be executed ...")
     command = (
         "jupyter nbconvert --to markdown "
@@ -35,12 +33,14 @@ def test_tf(container, test_file):
         "5132/tcp": 5132
     }) """
 
-
     c = container.run(
         volumes={host_data_dir: {"bind": cont_data_dir, "mode": "ro"}},
         tty=True,
         command=["start.sh", "bash", "-c", command],
     )
 
-    rv = c.wait(timeout=timeout_sec * 10)
+    rv = c.wait(timeout=timeout_sec//10 + 10)
+    logs = c.logs(stdout=True).decode("utf-8")
+    LOGGER.debug(logs)
+    print(logs)
     assert rv == 0 or rv["StatusCode"] == 0, f"Command {command} failed"
