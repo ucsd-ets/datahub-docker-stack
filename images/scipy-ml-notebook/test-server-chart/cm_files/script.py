e090739e35318f8933c9a65c4a583974a78b88ab
wesuuu <wuykimpang@ucsd.edu>
2023-02-14 10:58:10 -0800

    Updated grpc script

--- images/scipy-ml-notebook/test-server-chart/cm_files/script.py
+++ images/scipy-ml-notebook/test-server-chart/cm_files/script.py
@@ -3,6 +3,10 @@ try:
     import torch
     import json
     from functools import wraps
+    import numpy as np
+    from tensorflow.keras.models import Model
+    from tensorflow.keras.layers import Dense, Input
+
 except Exception as ex:
     print(f'Image missing a necessary package: {ex}')
     quit()
@@ -17,7 +21,25 @@ def log_errors(f):
 
 @log_errors
 def is_tensorflow_available(log):
+    # check GPUs
     log['tensorflow'] = len(tf.config.list_physical_devices('GPU')) > 0
+    
+    # random job
+    try:
+        X = np.random.random((100, 16))
+        y = np.random.randint(0, 2, size=(100,))
+        tf.config.set_visible_devices(gpus[0], 'GPU')
+        inputs = Input(shape=X.shape[1:])
+        x = Dense(4, activation="tanh")(inputs)
+        outputs = Dense(1, activation='sigmoid')(x)
+        model = Model(inputs=inputs, outputs=outputs)
+        model.compile(optimizer="sgd", loss="binary_crossentropy")
+
+        model.fit(X, y, verbose=1, epochs=1, batch_size=1)
+    
+    except Exception as e:
+        print(f'Tensorflow exception {e}')
+        return False
 
 @log_errors
 def is_torch_available(log):
