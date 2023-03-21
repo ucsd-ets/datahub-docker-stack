285ca731c0ba300f0201c9b2e76f991155255269
Thomaswang0822 <thomaswang0822@outlook.com>
2023-03-21 15:55:11 -0700

    remove unused doit tasks

--- dodo.py
+++ dodo.py
@@ -26,49 +26,6 @@ DOIT_CONFIG = dict(
 # get_var(<key>, <default_val>)
 USE_STACK = get_var('stack_dir', 'images')
 
-
-# REMOVE
-def task_unit_build():
-    """Build docker image and test it unit wise"""
-    # at runtime, configure real objects
-    build_retrieval = BuildInfoRetrieval(retrieval_func=get_build_info_from_filesystem)
-    build_info_storage = BuildInfoStorage(images_built_func=store_images_on_filesystem)
-    container_builder = ContainerBuilder(container_builder_func=build_units)
-    container_tester = ContainerTester(container_tester_func=container_test)  # line 113
-    pusher_func=setup_pusher_func("etsjenkins", os.environ['DOCKERHUB_TOKEN'])
-    container_pusher = ContainerPusher(container_pusher_func=pusher_func)
-    container_deleter = ContainerDeleter(container_deleter_func=delete_docker_containers)
-
-    container_facade = ContainerFacade(
-        build_retrieval,
-        container_builder,
-        container_tester,   # tester: ContainerTester
-        container_pusher,
-        container_deleter,
-        build_info_storage
-    )
-    
-    return {
-        'actions':[build_unit],
-        'file_dep': ['artifacts/IMAGES_CHANGED'],
-        'targets': ['artifacts/builder-metainfo_unit.json', 'artifacts/IMAGES_BUILT_unit'],
-        'params':[
-            {
-                'name': 'stack_dir',
-                'short': 'd',
-                'long': 'stack_dir',
-                'default': 'images'
-            },
-            {
-                'name': 'container_facade',
-                'short': 'c',
-                'long': 'container_facade',
-                'default': container_facade
-            }
-        ],
-
-    }
-
 # KEEP
 def task_prep():
     """Prep directory for logs and artifacts"""
@@ -89,65 +46,6 @@ def task_prep():
         'targets': ['artifacts/.empty', 'logs/.empty', 'manifests/.empty']
     }
 
-
-# REMOVE
-def task_clear():
-
-    def _clear():
-        if os.path.exists('artifacts'):
-            rmtree('artifacts')
-        if os.path.exists('logs'):
-            rmtree('logs')
-    
-    return {
-        'actions': [_clear]
-    }
-
-
-# REMOVE
-def task_prebuild():
-    """Prepare a build stack"""
-    return {
-        'actions': [save_changed_images, save_git_info],
-        'file_dep': ['artifacts/.empty', 'logs/.empty'],
-        'targets': ['artifacts/IMAGES_CHANGED', 'artifacts/GIT_*']
-    }
-
-# REMOVE
-def task_untested_push():
-    """Push all built images that need testing elsewhere"""
-    return {
-        'actions': [run_untested_push],
-        'file_dep': ['artifacts/IMAGES_BUILT'],
-        'targets': ['artifacts/IMAGES_UNTESTED_PUSHED']
-    }
-
-
-# REMOVE
-def task_push():
-    """Push all built images"""
-    return {
-        'actions': [run_push],
-        'file_dep': ['artifacts/IMAGES_BUILT'],
-        'targets': ['artifacts/IMAGES_PUSHED']
-    }
-
-# REMOVE
-def task_manifest():
-    """Build image manifests for all built images"""
-    return {
-        'actions': [run_manifests],
-        'file_dep': ['artifacts/IMAGES_BUILT'],
-        'targets': ['manifests/*.md'],
-        'params':[
-            {
-                'name': 'stack_dir',
-                'short': 'd',
-                'long': 'stack_dir',
-                'default': 'images'
-            },
-        ],
-    }
 
 # KEEP
 def task_tag():
