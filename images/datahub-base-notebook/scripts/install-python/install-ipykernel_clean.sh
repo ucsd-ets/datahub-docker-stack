032438887daf881227fcf85684796f35559fa518
Paul Jamason <pjamason@ucsd.edu>
2023-03-21 21:14:47 -0700

    add missing comma in sed cmd

--- images/datahub-base-notebook/scripts/install-python/install-ipykernel_clean.sh
+++ images/datahub-base-notebook/scripts/install-python/install-ipykernel_clean.sh
@@ -6,5 +6,5 @@ ipython kernel install --name "python3_clean" \
                        --sys-prefix
 
 # Add to kernel config (set ENV PYTHONNOUSERSITE to 1)
-sed -i "s~\"python\"$~\"python\",\n \"env\": {\n  \"PYTHONNOUSERSITE\": \"1\"\n }~" \
+sed -i "s~\"python\",$~\"python\",\n \"env\": {\n  \"PYTHONNOUSERSITE\": \"1\"\n }~" \
        /opt/conda/share/jupyter/kernels/python3_clean/kernel.json
