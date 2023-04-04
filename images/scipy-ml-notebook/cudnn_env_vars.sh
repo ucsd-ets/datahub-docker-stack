25f2c63cc0150df4a22eeddbe1fcb5bdfdae2611
Rockford Mankini <rmankini@ucsd.edu>
2023-04-04 00:06:24 -0700

    add ld_lib_path in activate.sh script instead

--- images/scipy-ml-notebook/cudnn_env_vars.sh
+++ images/scipy-ml-notebook/cudnn_env_vars.sh
@@ -4,3 +4,4 @@
 cudnn_version=$(conda list 2> /dev/null | grep cudnn | awk -F' ' '{ print $2 "-" $3 }')
 
 export LD_LIBRARY_PATH=/opt/conda/pkgs/cudnn-$cudnn_version/lib/:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/conda/lib/python3.9/site-packages/nvidia/cudnn/lib
