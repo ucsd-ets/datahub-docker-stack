a1a5530a86cb3b2958c934b86773a1487a810dc3
Rockford Mankini <rmankini@ucsd.edu>
2023-04-04 09:22:46 -0700

    add lib to variable

--- images/scipy-ml-notebook/cudnn_env_vars.sh
+++ images/scipy-ml-notebook/cudnn_env_vars.sh
@@ -6,4 +6,4 @@ cudnn_version=$(conda list 2> /dev/null | grep cudnn | awk -F' ' '{ print $2 "-"
 export LD_LIBRARY_PATH=/opt/conda/pkgs/cudnn-$cudnn_version/lib/:${LD_LIBRARY_PATH}
 CUDNN_PATH=$(dirname $(python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))
 #export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/conda/lib/python3.9/site-packages/nvidia/cudnn/lib
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${CUDNN_PATH}
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${CUDNN_PATH}/lib
