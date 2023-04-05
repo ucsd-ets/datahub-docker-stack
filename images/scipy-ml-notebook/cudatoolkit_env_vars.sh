e672f8dfcce5403e8283db2508384c4a558ad863
Rockford Mankini <rmankini@ucsd.edu>
2023-04-05 09:44:59 -0700

    try linux magic stuff

--- images/scipy-ml-notebook/cudatoolkit_env_vars.sh
+++ images/scipy-ml-notebook/cudatoolkit_env_vars.sh
@@ -4,3 +4,10 @@
 cudatoolkit=$(conda list 2> /dev/null | grep cudatoolkit | awk -F' ' '{ print $2 "-" $3 }')
 
 export LD_LIBRARY_PATH=/opt/conda/pkgs/cudatoolkit-$cudatoolkit/lib:${LD_LIBRARY_PATH}
+
+mkdir -p $CONDA_DIR/etc/conda/activate.d
+printf 'export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_DIR/lib/\n' >> $CONDA_DIR/etc/conda/activate.d/env_vars.sh
+source $CONDA_DIR/etc/conda/activate.d/env_vars.sh
+# Copy libdevice file to the required path
+mkdir -p $CONDA_DIR/lib/nvvm/libdevice
+cp $CONDA_DIR/lib/libdevice.10.bc $CONDA_DIR/lib/nvvm/libdevice/
