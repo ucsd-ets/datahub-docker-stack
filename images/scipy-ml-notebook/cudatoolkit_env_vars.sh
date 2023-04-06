a374f1116ac2958cad0473e87e5843aa8a7bd7be
Rockford Mankini <rmankini@ucsd.edu>
2023-04-06 13:41:55 -0700

    minor change to env vars

--- images/scipy-ml-notebook/cudatoolkit_env_vars.sh
+++ images/scipy-ml-notebook/cudatoolkit_env_vars.sh
@@ -5,9 +5,8 @@ cudatoolkit=$(conda list 2> /dev/null | grep cudatoolkit | awk -F' ' '{ print $2
 
 export LD_LIBRARY_PATH=/opt/conda/pkgs/cudatoolkit-$cudatoolkit/lib:${LD_LIBRARY_PATH}
 
-mkdir -p $CONDA_DIR/etc/conda/activate.d
-printf 'export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_DIR/lib/\n' >> $CONDA_DIR/etc/conda/activate.d/env_vars.sh
-source $CONDA_DIR/etc/conda/activate.d/env_vars.sh
+# pathing for ptxas/libdevice. see https://www.tensorflow.org/install/pip#linux
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_DIR/lib/
 # Copy libdevice file to the required path
 mkdir -p $CONDA_DIR/lib/nvvm/libdevice
 cp $CONDA_DIR/lib/libdevice.10.bc $CONDA_DIR/lib/nvvm/libdevice/
