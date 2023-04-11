7ff597298c136333cd4ad644e29c926b70520034
Rockford Mankini <rmankini@ucsd.edu>
2023-04-11 10:56:06 -0700

    replace / with -s in branch name

--- scripts/main.py
+++ scripts/main.py
@@ -14,7 +14,7 @@ def main(dockerhub_username: str, dockerhub_password: str):
     tag_prefix = spec['tag']['prefix']
 
     images_changed = git_helper.get_changed_images()
-    git_hash = git_helper.GitHelper.get_branch_name()
+    git_hash = git_helper.GitHelper.get_branch_name().replace("/", "-")
 
     root = build_tree(
         spec_yaml=spec, images_changed=images_changed, git_suffix=git_hash)
