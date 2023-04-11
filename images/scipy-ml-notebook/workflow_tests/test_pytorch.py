9962fc295b7415b6b0c0afe07ff369983304109f
Hanzen Shou <heshou@ucsd.edu>
2023-04-11 14:03:53 -0700

    Removed dead cuda empty cache calls
    also autoformatted the file

--- images/scipy-ml-notebook/workflow_tests/test_pytorch.py
+++ images/scipy-ml-notebook/workflow_tests/test_pytorch.py
@@ -1,19 +1,22 @@
 import torch
 
+
 def can_access_cuda():
-    return("Torch can access CUDA: " + str(torch.cuda.is_available()))
+    return ("Torch can access CUDA: " + str(torch.cuda.is_available()))
+
 
 def random_vector():
     # Set fixed seed
     torch.manual_seed(0)
 
     # Generate "random" matrix
-    outList = torch.randn(3,9).tolist()
+    outList = torch.randn(3, 9).tolist()
 
-    if (outList == [[-1.1258398294448853, -1.152360200881958, -0.2505785822868347, -0.4338788092136383, 0.8487103581428528, 0.6920091509819031, -0.31601276993751526, -2.1152193546295166, 0.32227492332458496], [-1.2633347511291504, 0.34998318552970886,0.26604941487312317, 0.16645534336566925, 0.8743818402290344, -0.14347384870052338, -0.1116093322634697, -0.6135830879211426,0.03159274160861969], [-0.4926769733428955, 0.05373690277338028, 0.6180566549301147, -0.41280221939086914, -0.8410648107528687, -2.316041946411133, -0.10230965167284012, 0.7924439907073975, -0.28966769576072693]]):
-        return("Test passed!")
+    if (outList == [[-1.1258398294448853, -1.152360200881958, -0.2505785822868347, -0.4338788092136383, 0.8487103581428528, 0.6920091509819031, -0.31601276993751526, -2.1152193546295166, 0.32227492332458496], [-1.2633347511291504, 0.34998318552970886, 0.26604941487312317, 0.16645534336566925, 0.8743818402290344, -0.14347384870052338, -0.1116093322634697, -0.6135830879211426, 0.03159274160861969], [-0.4926769733428955, 0.05373690277338028, 0.6180566549301147, -0.41280221939086914, -0.8410648107528687, -2.316041946411133, -0.10230965167284012, 0.7924439907073975, -0.28966769576072693]]):
+        return ("Test passed!")
     else:
         raise Exception("Test failed...output was " + str(outList))
+
 
 def matrix_multiplication():
     # Test 2 - Element-wise multiplication w/CUDA
@@ -31,14 +34,13 @@ def matrix_multiplication():
     c = a * b
     outList = c.tolist()
     if (outList == [4.0, 10.0, 18.0]):
-        return("Test passed!")
+        return ("Test passed!")
     else:
         raise Exception("Test failed...output was " + str(outList))
-        
-    # Empties Torch CUDA Cache
-    torch.cuda.empty_cache()
-    # NOTE: If you encounter CUDA error: out of memory, please restart your docker container. 
+
+    # NOTE: If you encounter CUDA error: out of memory, please restart your docker container.
     # The current assigned GPU is out of free NVRAM.
+
 
 def neural_network():
     # Test 3 - Define basic neural network w/CUDA
@@ -49,11 +51,12 @@ def neural_network():
     class Net(nn.Module):
         def __init__(self):
             super(Net, self).__init__()
-            self.fc1 = nn.Linear(2, 4) # Input layer has 2 nodes, hidden layer has 4 nodes
+            # Input layer has 2 nodes, hidden layer has 4 nodes
+            self.fc1 = nn.Linear(2, 4)
             # Set weight and bias to fixed values
             self.fc1.weight.data.fill_(1.0)
             self.fc1.bias.data.fill_(0.0)
-            self.fc2 = nn.Linear(4, 1) # Output layer has 1 node
+            self.fc2 = nn.Linear(4, 1)  # Output layer has 1 node
             # Set weight and bias to fixed values
             self.fc2.weight.data.fill_(1.0)
             self.fc2.bias.data.fill_(0.0)
@@ -86,12 +89,13 @@ def neural_network():
 
     # Check output
     if (outList == [[12.0]]):
-        return("Test passed!")
+        return ("Test passed!")
     else:
         raise Exception("Test failed...output was " + str(outList))
 
     # Empties Torch CUDA Cache
     torch.cuda.empty_cache()
+
 
 def length_of_dataset_no_cuda():
     # Test 4 - Calculate dataset length w/out CUDA
@@ -102,17 +106,16 @@ def length_of_dataset_no_cuda():
     transform = transforms.ToTensor()
 
     # Download and load the training data
-    train_data = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    train_data = datasets.MNIST(
+        root='./data', train=True, download=True, transform=transform)
 
     # Check the size of the training set
     ld = len(train_data)
     if (ld == 60000):
-        return("Test passed!")
+        return ("Test passed!")
     else:
         raise Exception("Test failed...output was " + str(ld))
-        
-    # Empties Torch CUDA Cache
-    torch.cuda.empty_cache()
+
 
 def mean_pixel_value_cuda():
     # Test 5 - Calculate mean pixel values from MNIST dataset w/CUDA
@@ -126,13 +129,18 @@ def mean_pixel_value_cuda():
         raise Exception("CUDA is not accessible/GPU not found. Test failed...")
 
     # Load MNIST dataset
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
-    train_set = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-    test_set = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+    train_set = datasets.MNIST(
+        root='./data', train=True, download=True, transform=transform)
+    test_set = datasets.MNIST(
+        root='./data', train=False, download=True, transform=transform)
 
     # Move dataset to device
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=64, shuffle=True, pin_memory=True, num_workers=2)
-    test_loader = torch.utils.data.DataLoader(test_set, batch_size=64, shuffle=True, pin_memory=True, num_workers=2)
+    train_loader = torch.utils.data.DataLoader(
+        train_set, batch_size=64, shuffle=True, pin_memory=True, num_workers=2)
+    test_loader = torch.utils.data.DataLoader(
+        test_set, batch_size=64, shuffle=True, pin_memory=True, num_workers=2)
 
     # Calculate mean value of dataset
     mean = torch.mean(train_set.data.float() / 255)
@@ -143,12 +151,10 @@ def mean_pixel_value_cuda():
     # Check truncated value
     trunc = round(output, 10)
     if (trunc == 0.1306604743):
-        return("Test passed!")
+        return ("Test passed!")
     else:
         raise Exception("Test failed...(truncated) output was " + str(trunc))
-        
-    # Empties Torch CUDA Cache
-    torch.cuda.empty_cache()
+
 
 def multiply_dataset_calculate_mean_cuda():
     # Test 6 - Multiplies MNIST dataset by 2, calculates mean of first 100 elements w/CUDA
@@ -163,15 +169,19 @@ def multiply_dataset_calculate_mean_cuda():
         raise Exception("CUDA is not accessible/GPU not found. Test failed...")
 
     # Download the MNIST dataset nb
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-    train_dataset = datasets.MNIST('./data', train=True, download=True, transform=transform)
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+    train_dataset = datasets.MNIST(
+        './data', train=True, download=True, transform=transform)
 
     # Create a DataLoader for the dataset
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=64, shuffle=True)
 
     # Move the data to GPU
     device = torch.device('cuda')
-    train_loader = [(data.to(device), target.to(device)) for data, target in train_loader]
+    train_loader = [(data.to(device), target.to(device))
+                    for data, target in train_loader]
 
     # Apply a simple operation using GPU
     for batch_idx, (data, target) in enumerate(train_loader):
@@ -179,36 +189,43 @@ def multiply_dataset_calculate_mean_cuda():
         if batch_idx == 0:
             exactFloat = torch.mean(target[:100].float()).item()
             if (exactFloat == 4.734375):
-                return("Test passed!")
+                return ("Test passed!")
             else:
                 raise Exception("Test failed...output was " + str(exactFloat))
-                
+
     # Empties Torch CUDA Cache
     torch.cuda.empty_cache()
+
 
 def test_can_access_cuda():
     result = can_access_cuda()
     assert result == "Torch can access CUDA: True"
 
+
 def test_random_vector():
     result = random_vector()
     assert result == "Test passed!"
+
 
 def test_matrix_multiplication():
     result = matrix_multiplication()
     assert result == "Test passed!"
 
+
 def test_neural_network():
     result = neural_network()
     assert result == "Test passed!"
+
 
 def test_length_of_dataset_no_cuda():
     result = length_of_dataset_no_cuda()
     assert result == "Test passed!"
 
+
 def test_mean_pixel_value_cuda():
     result = mean_pixel_value_cuda()
     assert result == "Test passed!"
+
 
 def test_multiply_dataset_calculate_mean_cuda():
     result = multiply_dataset_calculate_mean_cuda()
