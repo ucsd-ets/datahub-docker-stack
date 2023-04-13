33a93d3ad64b180ebbb6ba022ed20208738c1517
Thomaswang0822 <thomaswang0822@outlook.com>
2023-04-13 12:48:31 -0700

    finer-grained wait time; everything guard in try-excpet

--- images/rstudio-notebook/integration_tests/test_rstudio_ui.py
+++ images/rstudio-notebook/integration_tests/test_rstudio_ui.py
@@ -24,7 +24,10 @@ LOGGER = get_logger()
 
 THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 
-WAIT_TIME = 60 or os.environ.get('WAIT_TIME')
+WAIT_TIME = 60 or os.environ.get('WAIT_TIME')   # long break when loading sth;
+SLEEP_TIME = 5    # short break between operations
+FIND_TIME = 10     # tolerence when driver (we call it browser) find element
+
 MAX_RETRIES = 5 or os.environ.get('MAX_RETRIES')
 JUPYTER_TOKEN = os.environ.get('JUPYTER_TOKEN')
 SERVICE_NAME = '127.0.0.1' or os.environ.get('SERVICE_NAME')
@@ -55,6 +58,7 @@ def test_rstudio(container):
     options.add_argument("--disable-popup-blocking")
 
     browser = Chrome(options=options)
+    browser.implicitly_wait(FIND_TIME)  # global setting
 
     # give it some time for nbconvert to run and to spin up notebook server
     baseurl = 'http://{0}:8888'.format(SERVICE_NAME)
@@ -66,29 +70,31 @@ def test_rstudio(container):
     
     LOGGER.info(f"Rstudio UI test: container status: {c.status}")
     LOGGER.info(f"Rstudio UI test: container log: {c.logs()}")
-    LOGGER.info(f'CHECK WAIT_TIME: {WAIT_TIME}')
+    LOGGER.debug(f'CHECK WAIT_TIME: {WAIT_TIME}')
+    LOGGER.debug(f'CHECK SLEEP_TIME: {SLEEP_TIME}')
+    LOGGER.debug(f'CHECK FIND_TIME: {FIND_TIME}')
+
     current_retries = 0
-    while True:
-
-        if current_retries == MAX_RETRIES:
-            raise Exception('Max retry limit hit, could not connect to jupyter server')
-        
-        browser.get(baseurl)
-
-        if browser.page_source != '<html><head></head><body></body></html>':
-            break
-
-        current_retries += 1
-        LOGGER.info('Could not connect to server at {0} yet... Retry count = {1}'.format(baseurl, current_retries))
-        LOGGER.info(browser.page_source)
-        time.sleep(WAIT_TIME)
-
-    LOGGER.info('Connected to jupyter notebook')
-
-    # check only 1 tab
-    assert len(browser.window_handles) == 1
-
+    # pack everything inside a try-catch. Screenshot when an error is thrown
     try:
+        while True:
+            if current_retries == MAX_RETRIES:
+                raise Exception('Max retry limit hit, could not connect to jupyter server')
+            
+            browser.get(baseurl)
+
+            if browser.page_source != '<html><head></head><body></body></html>':
+                break
+
+            current_retries += 1
+            LOGGER.info('Could not connect to server at {0} yet... Retry count = {1}'.format(baseurl, current_retries))
+            LOGGER.info(browser.page_source)
+            time.sleep(SLEEP_TIME)
+        LOGGER.info('Connected to jupyter notebook')
+
+        # check only 1 tab
+        assert len(browser.window_handles) == 1
+
         # select the new button + create a python notebook
         LOGGER.info('Checking RStudio')
         new_button = webdriverwait(browser, WAIT_TIME).until(
@@ -100,6 +106,70 @@ def test_rstudio(container):
             ec.element_to_be_clickable((by.LINK_TEXT, 'RStudio'))
         )
         rstudio_button.click()
+
+        time.sleep(WAIT_TIME)   # for Rstudio Window/Tab to load
+
+        original_window = browser.current_window_handle
+        # check 2 tab
+        assert len(browser.window_handles) == 2
+        for window_handle in browser.window_handles:
+            # just switch to the other
+            if window_handle != original_window:
+                browser.switch_to.window(window_handle)
+                break
+        LOGGER.info('Switched to RStudio tab')
+        
+        LOGGER.info('Loading datascience-rstudio.Rmd')
+        rstudio_handler = browser.current_window_handle
+        ActionChains(browser).key_down(Keys.CONTROL).send_keys("o").perform()
+        browser.switch_to.window(browser.window_handles[-1])
+        ids = browser.find_elements(by.XPATH,'//*[@id]')
+        file_id=None
+        for ii in ids:
+            try:
+                e=ii.get_attribute('id')
+                if 'rstudio_dirContents' in e:
+                    if ii.text.split()[0]== 'datascience-rstudio.Rmd':
+                        file_id = e
+            except:
+                pass
+         
+        file_promtp=browser.find_element(by.ID,file_id)
+        file_promtp.click()
+        file_close = browser.find_element(by.XPATH,'//*[@id="rstudio_file_accept_open"]')
+        file_close.click()
+        LOGGER.info('datascience-rstudio.Rmd ok')
+        time.sleep(SLEEP_TIME)
+        
+        LOGGER.info('Checking knit')
+        ActionChains(browser).key_down(Keys.CONTROL).key_down(Keys.SHIFT).send_keys("k").perform()
+        LOGGER.info('knit clicked worked')
+        
+        time.sleep(SLEEP_TIME)
+        browser.save_screenshot('artifacts/screenshot.png')
+        notebook = browser.window_handles[0]
+        browser.switch_to.window(notebook)
+
+        # check for the pdf generated
+        exit_code,output = c.exec_run('ls /home/jovyan')
+        LOGGER.info(output.decode("utf-8"))
+        if 'datascience-rstudio.pdf' not in output.decode("utf-8") :
+            LOGGER.info('datascience-rstudio.pdf not generated')
+            raise FileNotFoundError('datascience-rstudio.pdf not generated')
+        
+        # select the quit button
+        LOGGER.info('Checking the quit button')
+        file = browser.window_handles[0]
+        browser.switch_to.window(file)
+        quit_btn = webdriverwait(browser, WAIT_TIME).until(
+            ec.element_to_be_clickable((by.ID, 'shutdown'))
+        )
+        quit_btn.click()
+
+        time.sleep(SLEEP_TIME)
+        LOGGER.info('Exited the notebook server')
+        LOGGER.info('RStudio UI test: all pass!')
+
     except Exception as e:
         # save screenshot
         ss_name = 'error_ss.png'
@@ -111,66 +181,3 @@ def test_rstudio(container):
             for chunk in data:
                 local_file.write(chunk)
         raise Exception("Button Time out, check error screenshot in logs/")
-
-    time.sleep(WAIT_TIME)
-    LOGGER.info('RStudio ok')
-    LOGGER.info('Loading datascience-rstudio.Rmd')
-
-    original_window = browser.current_window_handle
-    for window_handle in browser.window_handles:
-        if window_handle != original_window:
-            browser.switch_to.window(window_handle)
-            break
-    
-    browser.implicitly_wait(WAIT_TIME)
-    rstudio_handler = browser.current_window_handle
-    ActionChains(browser).key_down(Keys.CONTROL).send_keys("o").perform()
-    browser.switch_to.window(browser.window_handles[-1])
-    ids = browser.find_elements(by.XPATH,'//*[@id]')
-    file_id=None
-    for ii in ids:
-        try:
-            e=ii.get_attribute('id')
-            if 'rstudio_dirContents' in e:
-                if ii.text.split()[0]== 'datascience-rstudio.Rmd':
-                    file_id = e
-        except:
-            pass
-         
-    file_promtp=browser.find_element(by.ID,file_id)
-    file_promtp.click()
-    file_close = browser.find_element(by.XPATH,'//*[@id="rstudio_file_accept_open"]')
-    file_close.click()
-    LOGGER.info('datascience-rstudio.Rmd ok')
-    time.sleep(WAIT_TIME)
-    
-    LOGGER.info('Checking knit')
-    
-    ActionChains(browser).key_down(Keys.CONTROL).key_down(Keys.SHIFT).send_keys("k").perform()
-    LOGGER.info('knit clicked worked')
-    
-    time.sleep(WAIT_TIME)
-    browser.save_screenshot('artifacts/screenshot.png')
-    notebook = browser.window_handles[0]
-    browser.switch_to.window(notebook)
-
-    # check for the pdf generated
-    exit_code,output = c.exec_run('ls /home/jovyan')
-    LOGGER.info(output.decode("utf-8"))
-    if  'datascience-rstudio.pdf' not  in output.decode("utf-8") :
-        LOGGER.info('datascience-rstudio.pdf not generated')
-        raise FileNotFoundError
-       
-
-
-    # select the quit button
-    LOGGER.info('Checking the quit button')
-    file = browser.window_handles[0]
-    browser.switch_to.window(file)
-    quit_btn = webdriverwait(browser, WAIT_TIME).until(
-        ec.element_to_be_clickable((by.ID, 'shutdown'))
-    )
-    quit_btn.click()
-
-    LOGGER.info('Exited the notebook server')
-    LOGGER.info('RStudio UI test: all pass!')
