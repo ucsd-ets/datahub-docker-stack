864b2442c0ac8d1a14e1889ef665e4255f09670e
3xpl0itz <127802411+dafeliton@users.noreply.github.com>
2023-04-19 21:26:47 -0700

    Remove R test copies from rstudio notebook

--- images/rstudio-notebook/test/test_r_dump_packages.R
+++
@@ -1,9 +0,0 @@
-# Get information about installed packages
-installed_packages <- installed.packages()
-
-# Get library paths and package names
-library_paths <- installed_packages[, "LibPath"]
-package_names <- rownames(installed_packages)
-
-# Print the package filepaths
-print(package_names)
