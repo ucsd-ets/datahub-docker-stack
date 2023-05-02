d1e849b9acd6010e5da43130678a40a6c79d45e7
Thomaswang0822 <thomaswang0822@outlook.com>
2023-05-02 15:17:23 -0700

    fetch chrome version at runtime instead of using (and frequently updating) Github Secrets; rebuild rstudio

--- scripts/selenium_setup.sh
+++ scripts/selenium_setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-# set -ex
-# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-# sudo apt install ./google-chrome-stable_current_amd64.deb
+
+# fetch signing key and install Chrome
 sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add 
 sudo bash -c "echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' >> /etc/apt/sources.list.d/google-chrome.list" 
 sudo apt -y update 
 sudo apt -y install google-chrome-stable 
-wget https://chromedriver.storage.googleapis.com/$1/chromedriver_linux64.zip
+
+# instead of using command-line arg passed in from Github Secrets,
+# we grab the Chrome version and download matching ChromeDriver
+chrome_version=$(google-chrome --version | grep -iE "[0-9.]{10,20}")
+echo "The Chrome version is: ${chrome_version}"
+wget https://chromedriver.storage.googleapis.com/${chrome_version}/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip 
 sudo mv chromedriver /usr/bin/chromedriver 
 sudo chown root:root /usr/bin/chromedriver 
