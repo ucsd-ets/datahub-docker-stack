28b85e4d2e11e980a37580c520fcf0bd5930624b
ThomasWang_OldB <56232866+Thomaswang0822@users.noreply.github.com>
2023-05-02 18:32:16 -0700

    Chrome version fix (#64)
    * Solve the incompatibility between auto-updated Chrome version and manually updated Chrome Driver version
    * Deprecate Github Secrets $CHROMEDRIVER_VERSION

--- scripts/selenium_setup.sh
+++ scripts/selenium_setup.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-# set -ex
-# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-# sudo apt install ./google-chrome-stable_current_amd64.deb
-sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add 
-sudo bash -c "echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' >> /etc/apt/sources.list.d/google-chrome.list" 
-sudo apt -y update 
+
+# File google-chrome.list allows `apt update` to fetch the latest stable of Chrome
+sudo bash -c "echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' >> /etc/apt/sources.list.d/google-chrome.list"
+sudo apt -y update
+# Install latest Chrome
 sudo apt -y install google-chrome-stable 
-wget https://chromedriver.storage.googleapis.com/$1/chromedriver_linux64.zip
+chrome_version=$(grep -iEo "[0-9.]{10,20}" <(google-chrome --version))
+echo "The stable Chrome version is: ${chrome_version}"
+
+# ISSUE: Chrome driver may not have the same latest version for download
+# Workaround: fetch the LATEST_RELEASE version available for download
+driver_version=$(curl -s -L https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+echo "Latest Chrome driver version is: ${driver_version}"
+wget https://chromedriver.storage.googleapis.com/${driver_version}/chromedriver_linux64.zip
+
+# install Chrome Driver
 unzip chromedriver_linux64.zip 
 sudo mv chromedriver /usr/bin/chromedriver 
 sudo chown root:root /usr/bin/chromedriver 
 sudo chmod +x /usr/bin/chromedriver
-pip install selenium
