6835cdccaedfde2eb85c7977b962f0d359137e69
heshou198 <129102271+heshou198@users.noreply.github.com>
2023-05-11 15:51:10 -0700

    Update cudatoolkit_env_vars.sh
    Swapped out conda list for lighter ls call

--- images/scipy-ml-notebook/cudatoolkit_env_vars.sh
+++ images/scipy-ml-notebook/cudatoolkit_env_vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # set systemwide env variables for Conda package: cudnn
-cudatoolkit=$(conda list 2> /dev/null | grep cudatoolkit | awk -F' ' '{ print $2 "-" $3 }')
+cudatoolkit=$(ls /opt/conda/pkgs | grep cudatoolkit | head -1 | cut -d '-' -f 2-)
 
 export LD_LIBRARY_PATH=/opt/conda/pkgs/cudatoolkit-$cudatoolkit/lib:${LD_LIBRARY_PATH}
 
