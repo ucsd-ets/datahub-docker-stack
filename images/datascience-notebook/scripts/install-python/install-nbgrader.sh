0ab444bd401d163d67147077af7d0de9e8d44690
Rockford Mankini <rmankini@ucsd.edu>
2023-08-16 18:05:45 -0700

    upgrade nbgrader

--- images/datascience-notebook/scripts/install-python/install-nbgrader.sh
+++ images/datascience-notebook/scripts/install-python/install-nbgrader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip install nbgrader==0.8.1
+pip install nbgrader==0.8.4
 
 jupyter nbextension install --symlink --sys-prefix --py nbgrader
 jupyter nbextension enable --sys-prefix --py nbgrader
