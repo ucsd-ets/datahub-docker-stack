21ac4f1981c66f9d9ffb9ecd98b4df75105c9932
Rockford Mankini <rmankini@ucsd.edu>
2023-08-18 16:50:01 -0700

    remove evil tensorrt.

--- images/scipy-ml-notebook/tensorrt_env_vars.sh
+++ images/scipy-ml-notebook/tensorrt_env_vars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # put tensorrt lib into LD_LIBRARY_PATH
-export LD_LIBRARY_PATH="/opt/conda/lib/python3.9/site-packages/tensorrt_libs:$LD_LIBRARY_PATH"
+##export LD_LIBRARY_PATH="/opt/conda/lib/python3.9/site-packages/tensorrt_libs:$LD_LIBRARY_PATH"
