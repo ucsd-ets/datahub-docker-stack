8e2a02ccfe43c1c8f72b9efa14f8053a291a372a
Rockford Mankini <rmankini@ucsd.edu>
2023-11-30 11:48:06 -0800

    try to install notebook 7

--- images/datascience-notebook/scripts/install-python/install-nbgrader.sh
+++ images/datascience-notebook/scripts/install-python/install-nbgrader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip install nbgrader==0.8.1
+pip install nbgrader==0.9.0
 
 jupyter nbextension install --symlink --sys-prefix --py nbgrader
 jupyter nbextension enable --sys-prefix --py nbgrader
