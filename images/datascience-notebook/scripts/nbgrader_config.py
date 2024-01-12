5640571caa0d56cc7fcf27e8c9dabc2ecd31fcfa
RockfordMankiniUCSD <92760584+RockfordMankiniUCSD@users.noreply.github.com>
2024-01-12 12:47:08 -0800

    Update nbgrader_config.py

--- images/datascience-notebook/scripts/nbgrader_config.py
+++ images/datascience-notebook/scripts/nbgrader_config.py
@@ -10,11 +10,3 @@ c.Exchange.course_id = course_id
 c.Exchange.root = "/srv/nbgrader/exchange"
 
 c.ExecutePreprocessor.timeout = 300
-
-c.ClearSolutions.begin_solution_delimeter = "BEGIN MY SOLUTION"
-c.ClearSolutions.end_solution_delimeter = "END MY SOLUTION"
-c.ClearSolutions.code_stub = {
-    "R": "# your code here\nfail() # No Answer - remove if you provide an answer",
-    "python": "# your code here\nraise NotImplementedError",
-    "javascript": "// your code here\nthrow new Error();"
-}
