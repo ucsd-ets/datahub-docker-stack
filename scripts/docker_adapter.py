1cfd03b0c5ca9abe4479380c8b962a6677945f63
swapnil sinha <swapnilsinha33@gmail.com>
2024-02-06 09:59:52 -0800

    test

--- scripts/docker_adapter.py
+++ scripts/docker_adapter.py
@@ -70,6 +70,7 @@ def build(node: Node) -> Tuple[bool, str]:
         ):
             # line is of type dict
             content_str = line.get('stream', '').strip()    # sth like 'Step 1/20 : ARG PYTHON_VERSION=python-3.9.5'
+            logger.info(content_str)
             if content_str:     # if not empty string
                 # time each major step (Step 1/23 : xxx)
                 if content_str[:4] == "Step":
@@ -79,7 +80,6 @@ def build(node: Node) -> Tuple[bool, str]:
 
                 report += content_str + '\n'
         # time for last step
-        logger.info(f"Now we have these images:")
         last_t, m, s = get_time_duration(last_t)
         report += f'Step {step} took [{m} min {s} sec] \n'
         logger.info(f"Now we have these images: { __docker_client.images.list()}")
