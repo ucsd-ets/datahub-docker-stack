82a9c8024861c720a2e92ca622319961b49bcd70
dafeliton <dafeliton@ucsd.edu>
2024-02-26 19:03:31 -0800

    Bump everything

--- images/scipy-ml-notebook/tensorrt_env_vars.sh
+++ images/scipy-ml-notebook/tensorrt_env_vars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # put tensorrt lib into LD_LIBRARY_PATH
-export LD_LIBRARY_PATH="/opt/conda/lib/python3.9/site-packages/tensorrt:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/opt/conda/lib/python3.11/site-packages/tensorrt:$LD_LIBRARY_PATH"
