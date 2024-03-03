f3a3fa95466896ff873a6a3021fa573c52ea4a49
dafeliton <dafeliton@ucsd.edu>
2024-03-03 13:13:05 -0800

    investigate cuda-toolkit, cleanup

--- images/scipy-ml-notebook/cudatoolkit_env_vars.sh
+++ images/scipy-ml-notebook/cudatoolkit_env_vars.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # set systemwide env variables for Conda package: cudnn
-cudatoolkit=$(ls /opt/conda/pkgs | grep cudatoolkit | head -1 | cut -d '-' -f 2-)
+#cudatoolkit=$(ls /opt/conda/pkgs | grep cudatoolkit | head -1 | cut -d '-' -f 2-)
 
-export LD_LIBRARY_PATH=/opt/conda/pkgs/cudatoolkit-$cudatoolkit/lib:${LD_LIBRARY_PATH}
+#export LD_LIBRARY_PATH=/opt/conda/pkgs/cudatoolkit-$cudatoolkit/lib:${LD_LIBRARY_PATH}
 
 # pathing for ptxas/libdevice. see https://www.tensorflow.org/install/pip#linux
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_DIR/lib/
