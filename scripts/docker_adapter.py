d68a0f5fbe841bf429a620f443c6cac95e44f91f
dafeliton <dafeliton@ucsd.edu>
2024-03-06 11:45:08 -0800

    Test bad apt call

--- scripts/docker_adapter.py
+++ scripts/docker_adapter.py
@@ -73,13 +73,18 @@ def build(node: Node) -> Tuple[bool, str]:
         ):
             # line is of type dict
             content_str = line.get('stream', '').strip()    # sth like 'Step 1/20 : ARG PYTHON_VERSION=python-3.9.5'
+            error_str = line.get('error', '').strip()
+            
+            if error_str:
+                logger.error("Test!")
+                raise docker_client.errors.BuildError(build_log=error_str, reason=error_str)
+            
             if content_str:     # if not empty string
                 # time each major step (Step 1/23 : xxx)
                 if content_str[:4] == "Step":
                     last_t, m, s = get_time_duration(last_t)
                     report += f'Step {step} took [{m} min {s} sec] \n'
                     step += 1
-
                 report += content_str + '\n'
                 
                 # Error detection. The docker client is not throwing errors if the build fails.
