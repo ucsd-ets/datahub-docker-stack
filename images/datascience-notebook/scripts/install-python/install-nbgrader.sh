6366ae519da226d0e60f4c3ad9836f7dc287d5d8
dafeliton <dafeliton@ucsd.edu>
2024-07-15 16:51:23 -0700

    nbgrader 0.9.3, fix validate button

--- images/datascience-notebook/scripts/install-python/install-nbgrader.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pip install nbgrader==0.9.1
-# >0.9 does not work with nbclassic...
-# nbclassic 
-#jupyter-nbclassic-extension install --symlink --sys-prefix --py nbgrader
-#jupyter-nbclassic-extension enable --sys-prefix --py nbgrader
