2ca474347794375fdfe28c09bb151e1f7b7cc4f0
dafeliton <dafeliton@ucsd.edu>
2024-12-29 01:52:21 -0800

    fix tensorrt by ignoring it

--- images/scipy-ml-notebook/tensorrt_env_vars.sh
+++ images/scipy-ml-notebook/tensorrt_env_vars.sh
@@ -4,6 +4,6 @@
 #export LD_LIBRARY_PATH="/opt/conda/lib/python3.11/site-packages/tensorrt:$LD_LIBRARY_PATH"
 
 # tensorrt 8.6+
-export LD_LIBRARY_PATH="/opt/conda/lib/python3.11/site-packages/tensorrt:$LD_LIBRARY_PATH"
-export LD_LIBRARY_PATH="/opt/conda/lib/python3.11/site-packages/tensorrt_bindings:$LD_LIBRARY_PATH"
-export LD_LIBRARY_PATH="/opt/conda/lib/python3.11/site-packages/tensorrt_libs:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/opt/conda/lib/python3.12/site-packages/tensorrt:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/opt/conda/lib/python3.12/site-packages/tensorrt_bindings:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/opt/conda/lib/python3.12/site-packages/tensorrt_libs:$LD_LIBRARY_PATH"
