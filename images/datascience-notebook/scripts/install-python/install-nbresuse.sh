cb9b501fff2494b4095672a170ea6fd315f44ded
Rockford Mankini <rmankini@ucsd.edu>
2026-05-29 12:33:25 -0700

    remove direct git install of jupyter

--- images/datascience-notebook/scripts/install-python/install-nbresuse.sh
+++ images/datascience-notebook/scripts/install-python/install-nbresuse.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-pip install --no-cache-dir git+https://github.com/jupyter-server/jupyter-resource-usage
+pip install --no-cache-dir pip install jupyter-resource-usage
 #pip install --no-cache-dir git+https://github.com/ucsd-ets/nbresuse.git 
     # nbclassic
     #&& \ jupyter server extension enable --sys-prefix --py nbresuse && \
